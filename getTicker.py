c1eb782f701dd5f453fd71622554c96db1c0b093
Alexander El-Hajj <alexander.el-hajj@canada.ca>
2021-06-29 15:04:17 -0400

    adding files to repo

---
+++ getTicker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 14:30:42 2021
+
+@author: alexanderel-hajj
+"""
+
+
+
+import json
+
+
+data_path = "./cached"
+server_name = "814189734884409364_Rags To Riches Trading"
+room1 = "844627582074093598_rich-penny-swings"
+room2 = "838942842000637992_stonk-king-options"
+room3 = "817816874993451038_moneybags-daytrades"
+
+
+
+
+
